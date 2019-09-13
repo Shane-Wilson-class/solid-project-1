78fb0eb2f2bb649e585e9556aee45fb801686659
Damon German <Damon.German@hyland.com>
2019-09-13 12:32:12 -0500

    made some changes

---
+++ solid-project-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using LiteDB;
+using RP_Violation;
+
+namespace solid_project_1
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var tradeStream = File.OpenRead("trades.txt");
+            var tradeProcessor = new TradeProcessor();
+            tradeProcessor.ProcessTrades(tradeStream);
+
+            using (var db = new LiteRepository(@"trades.db"))
+            {
+                db.Query<TradeRecord>().ToList().ForEach(Console.WriteLine);
+            }
+        }
+    }
+}
---
+++ solid-project-1/TradeRecord.cs
@@ -0,0 +1,15 @@
+﻿namespace RP_Violation
+{
+    public class TradeRecord
+    {
+        public int Id { get; set; }
+        public string DestinationCurrency { get; set; }
+        public float Lots { get; set; }
+        public decimal Price { get; set; }
+        public string SourceCurrency { get; set; }
+        public override string ToString()
+        {
+            return $"ID:{Id}, SourceCur:{SourceCurrency}, DestCur:{DestinationCurrency}, Number of Lots:{Lots}, Price:{Price}";
+        }
+    }
+}
