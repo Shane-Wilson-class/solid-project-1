c2708de5eba4f46312f6dd26933835e06382e23e
Damon German <Damon.German@hyland.com>
2019-09-13 12:58:01 -0500

    made some changes

--- solid-project-1/TradeProcessor.cs
+++ solid-project-1/TradeProcessor.cs
@@ -2,20 +2,6 @@
 
 namespace solid_project_1
 {
-    /// <summary>
-    ///     The responsibilities of the TradeProcessor are reading streams, parsing strings, validating fields, logging, and
-    ///     database insertion. The single responsibility principle states that this class, like all others, should have only a
-    ///     single reason to change. However, the reality of the TradeProcessor is that it will change under the following
-    ///     circumstances:
-    ///     When you decide not to use a Stream for input but instead read the trades from a remote call to a web service.
-    ///     When the format of the input data changes, perhaps with the addition of an extra field indicating the broker for
-    ///     the transaction.
-    ///     When the validation rules of the input data change.
-    ///     When the way in which you log warnings, errors, and information changes.
-    ///     When the database changes in any way.
-    ///     Example from Adaptive Code: Agile coding with design patterns and SOLID principles 2nd Edition, by McLean Hall,
-    ///     Gary
-    /// </summary>
     public class TradeProcessor
     {
         private readonly ITradeParser _tradeParser;
@@ -37,9 +23,5 @@ namespace solid_project_1
 
             _tradeStorage.Persist(trades);
         }
-
-
-
-
     }
 }
