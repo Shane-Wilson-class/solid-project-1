cd2a91c61c07e16413844a0dbe4b9f782718ac1a
Damon German <damon.german@hyland.com>
2021-09-05 14:14:45 -0500

    Upgraded project removed unity

--- solid-project-1/Program.cs
+++ solid-project-1/Program.cs
@@ -1,25 +1,20 @@
 ﻿using System;
 using System.IO;
 using LiteDB;
-using Unity;
-
 
 namespace solid_project_1
 {
     internal static class Program
     {
-        //private static readonly UnityContainer Container = new UnityContainer(); 
-
         private static void Main()
         {
             var tradeStream = File.OpenRead("trades.txt");
             var tradeProcessor = new TradeProcessor();
             tradeProcessor.ProcessTrades(tradeStream);
 
-            using (var db = new LiteRepository(@"trades.db"))
-            {
-                db.Query<TradeRecord>().ToList().ForEach(Console.WriteLine);
-            }
+            using var db = new LiteRepository(@"trades.db");
+
+            db.Query<TradeRecord>().ToList().ForEach(Console.WriteLine);
         }
     }
 }
