46cdba17896a4f5a158b1893744a81aeed3d4044
Damon German <49420596+dgerman3@users.noreply.github.com>
2025-09-14 11:25:23 -0500

    refactor: Clean up implementation and interface contract tests for better readability and maintainability

--- solid-project-1.Tests/ImplementationTests.cs
+++ solid-project-1.Tests/ImplementationTests.cs
@@ -1,107 +1,102 @@
-using System;
-using System.Reflection;
-using NUnit.Framework;
+namespace solid_project_1.Tests;
 
-namespace solid_project_1.Tests
+/// <summary>
+/// Tests to verify that implementation classes exist and implement the correct interfaces.
+/// These tests help ensure you've created the implementation classes as specified.
+/// </summary>
+[TestFixture]
+public class ImplementationTests
 {
-    /// <summary>
-    /// Tests to verify that implementation classes exist and implement the correct interfaces.
-    /// These tests help ensure you've created the implementation classes as specified.
-    /// </summary>
-    [TestFixture]
-    public class ImplementationTests
+    [Test]
+    public void TradeDataProvider_Class_Should_Exist()
     {
-        [Test]
-        public void TradeDataProvider_Class_Should_Exist()
-        {
-            var classType = Type.GetType("solid_project_1.TradeDataProvider, solid-project-1");
+        var classType = Type.GetType("solid_project_1.TradeDataProvider, solid-project-1");
 
-            Assert.That(classType, Is.Not.Null);
-            Assert.That(classType.IsClass, Is.True);
-            Assert.That(classType.IsAbstract, Is.False);
-        }
+        Assert.That(classType, Is.Not.Null);
+        Assert.That(classType.IsClass, Is.True);
+        Assert.That(classType.IsAbstract, Is.False);
+    }
 
-        [Test]
-        public void TradeDataProvider_Should_Implement_ITradeDataProvider()
-        {
-            var classType = Type.GetType("solid_project_1.TradeDataProvider, solid-project-1");
-            var interfaceType = Type.GetType("solid_project_1.ITradeDataProvider, solid-project-1");
+    [Test]
+    public void TradeDataProvider_Should_Implement_ITradeDataProvider()
+    {
+        var classType = Type.GetType("solid_project_1.TradeDataProvider, solid-project-1");
+        var interfaceType = Type.GetType("solid_project_1.ITradeDataProvider, solid-project-1");
 
-            Assert.That(classType, Is.Not.Null);
-            Assert.That(interfaceType, Is.Not.Null);
-            Assert.That(interfaceType.IsAssignableFrom(classType), Is.True);
-        }
+        Assert.That(classType, Is.Not.Null);
+        Assert.That(interfaceType, Is.Not.Null);
+        Assert.That(interfaceType.IsAssignableFrom(classType), Is.True);
+    }
 
-        [Test]
-        public void TradeParser_Class_Should_Exist()
-        {
-            var classType = Type.GetType("solid_project_1.TradeParser, solid-project-1");
+    [Test]
+    public void TradeParser_Class_Should_Exist()
+    {
+        var classType = Type.GetType("solid_project_1.TradeParser, solid-project-1");
 
-            Assert.That(classType, Is.Not.Null);
-            Assert.That(classType.IsClass, Is.True);
-            Assert.That(classType.IsAbstract, Is.False);
-        }
+        Assert.That(classType, Is.Not.Null);
+        Assert.That(classType.IsClass, Is.True);
+        Assert.That(classType.IsAbstract, Is.False);
+    }
 
-        [Test]
-        public void TradeParser_Should_Implement_ITradeParser()
-        {
-            var classType = Type.GetType("solid_project_1.TradeParser, solid-project-1");
-            var interfaceType = Type.GetType("solid_project_1.ITradeParser, solid-project-1");
+    [Test]
+    public void TradeParser_Should_Implement_ITradeParser()
+    {
+        var classType = Type.GetType("solid_project_1.TradeParser, solid-project-1");
+        var interfaceType = Type.GetType("solid_project_1.ITradeParser, solid-project-1");
 
-            Assert.That(classType, Is.Not.Null);
-            Assert.That(interfaceType, Is.Not.Null);
-            Assert.That(interfaceType.IsAssignableFrom(classType), Is.True);
-        }
+        Assert.That(classType, Is.Not.Null);
+        Assert.That(interfaceType, Is.Not.Null);
+        Assert.That(interfaceType.IsAssignableFrom(classType), Is.True);
+    }
 
-        [Test]
-        public void TradeStorage_Class_Should_Exist()
-        {
-            var classType = Type.GetType("solid_project_1.TradeStorage, solid-project-1");
+    [Test]
+    public void TradeStorage_Class_Should_Exist()
+    {
+        var classType = Type.GetType("solid_project_1.TradeStorage, solid-project-1");
 
-            Assert.That(classType, Is.Not.Null);
-            Assert.That(classType.IsClass, Is.True);
-            Assert.That(classType.IsAbstract, Is.False);
-        }
+        Assert.That(classType, Is.Not.Null);
+        Assert.That(classType.IsClass, Is.True);
+        Assert.That(classType.IsAbstract, Is.False);
+    }
 
-        [Test]
-        public void TradeStorage_Should_Implement_ITradeStorage()
-        {
-            var classType = Type.GetType("solid_project_1.TradeStorage, solid-project-1");
-            var interfaceType = Type.GetType("solid_project_1.ITradeStorage, solid-project-1");
+    [Test]
+    public void TradeStorage_Should_Implement_ITradeStorage()
+    {
+        var classType = Type.GetType("solid_project_1.TradeStorage, solid-project-1");
+        var interfaceType = Type.GetType("solid_project_1.ITradeStorage, solid-project-1");
 
-            Assert.That(classType, Is.Not.Null);
-            Assert.That(interfaceType, Is.Not.Null);
-            Assert.That(interfaceType.IsAssignableFrom(classType), Is.True);
-        }
+        Assert.That(classType, Is.Not.Null);
+        Assert.That(interfaceType, Is.Not.Null);
+        Assert.That(interfaceType.IsAssignableFrom(classType), Is.True);
+    }
 
-        [Test]
-        public void TradeProcessor_Should_Not_Be_Static_Class()
-        {
-            var classType = Type.GetType("solid_project_1.TradeProcessor, solid-project-1");
+    [Test]
+    public void TradeProcessor_Should_Not_Be_Static_Class()
+    {
+        var classType = Type.GetType("solid_project_1.TradeProcessor, solid-project-1");
 
-            Assert.That(classType, Is.Not.Null);
-            Assert.That(classType.IsClass, Is.True);
+        Assert.That(classType, Is.Not.Null);
+        Assert.That(classType.IsClass, Is.True);
 
-            var isStaticClass = classType is { IsAbstract: true, IsSealed: true };
-            Assert.That(isStaticClass, Is.False, "TradeProcessor should not be a static class after refactoring");
-        }
+        var isStaticClass = classType is { IsAbstract: true, IsSealed: true };
+        Assert.That(isStaticClass, Is.False, "TradeProcessor should not be a static class after refactoring");
+    }
 
-        [Test]
-        public void TradeProcessor_Should_Have_Constructor_With_Dependencies()
-        {
-            var classType = Type.GetType("solid_project_1.TradeProcessor, solid-project-1");
-            var tradeParserInterface = Type.GetType("solid_project_1.ITradeParser, solid-project-1");
-            var tradeStorageInterface = Type.GetType("solid_project_1.ITradeStorage, solid-project-1");
-            var tradeDataProviderInterface = Type.GetType("solid_project_1.ITradeDataProvider, solid-project-1");
+    [Test]
+    public void TradeProcessor_Should_Have_Constructor_With_Dependencies()
+    {
+        var classType = Type.GetType("solid_project_1.TradeProcessor, solid-project-1");
+        var tradeParserInterface = Type.GetType("solid_project_1.ITradeParser, solid-project-1");
+        var tradeStorageInterface = Type.GetType("solid_project_1.ITradeStorage, solid-project-1");
+        var tradeDataProviderInterface = Type.GetType("solid_project_1.ITradeDataProvider, solid-project-1");
 
-            var constructor = classType?.GetConstructor([
-                tradeParserInterface,
-                tradeStorageInterface,
-                tradeDataProviderInterface
-            ]);
+        var constructor = classType?.GetConstructor([
+            tradeParserInterface,
+            tradeStorageInterface,
+            tradeDataProviderInterface
+        ]);
 
-            Assert.That(constructor, Is.Not.Null);
-            Assert.That(constructor.GetParameters().Length, Is.EqualTo(3));
-        }
+        Assert.That(constructor, Is.Not.Null);
+        Assert.That(constructor.GetParameters().Length, Is.EqualTo(3));
     }
 }
--- solid-project-1.Tests/InterfaceContractTests.cs
+++ solid-project-1.Tests/InterfaceContractTests.cs
@@ -1,88 +1,81 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using NUnit.Framework;
+namespace solid_project_1.Tests;
 
-namespace solid_project_1.Tests
+/// <summary>
+/// Tests to verify that the required interfaces exist with correct method signatures.
+/// These tests help ensure you've created the interfaces as specified in the assignment.
+/// </summary>
+[TestFixture]
+public class InterfaceContractTests
 {
-    /// <summary>
-    /// Tests to verify that the required interfaces exist with correct method signatures.
-    /// These tests help ensure you've created the interfaces as specified in the assignment.
-    /// </summary>
-    [TestFixture]
-    public class InterfaceContractTests
+    [Test]
+    public void ITradeDataProvider_Interface_Should_Exist()
     {
-        [Test]
-        public void ITradeDataProvider_Interface_Should_Exist()
-        {
-            var interfaceType = Type.GetType("solid_project_1.ITradeDataProvider, solid-project-1");
+        var interfaceType = Type.GetType("solid_project_1.ITradeDataProvider, solid-project-1");
 
-            Assert.That(interfaceType, Is.Not.Null);
-            Assert.That(interfaceType.IsInterface, Is.True);
-        }
+        Assert.That(interfaceType, Is.Not.Null);
+        Assert.That(interfaceType.IsInterface, Is.True);
+    }
 
-        [Test]
-        public void ITradeDataProvider_Should_Have_GetTradeData_Method()
-        {
-            var interfaceType = Type.GetType("solid_project_1.ITradeDataProvider, solid-project-1");
+    [Test]
+    public void ITradeDataProvider_Should_Have_GetTradeData_Method()
+    {
+        var interfaceType = Type.GetType("solid_project_1.ITradeDataProvider, solid-project-1");
 
-            var method = interfaceType?.GetMethod("GetTradeData");
+        var method = interfaceType?.GetMethod("GetTradeData");
 
-            Assert.That(method, Is.Not.Null);
-            Assert.That(method.ReturnType, Is.EqualTo(typeof(List<string>)));
+        Assert.That(method, Is.Not.Null);
+        Assert.That(method.ReturnType, Is.EqualTo(typeof(List<string>)));
 
-            var parameters = method.GetParameters();
-            Assert.That(parameters.Length, Is.EqualTo(1));
-            Assert.That(parameters[0].ParameterType, Is.EqualTo(typeof(Stream)));
-        }
+        var parameters = method.GetParameters();
+        Assert.That(parameters.Length, Is.EqualTo(1));
+        Assert.That(parameters[0].ParameterType, Is.EqualTo(typeof(Stream)));
+    }
 
-        [Test]
-        public void ITradeParser_Interface_Should_Exist()
-        {
-            var interfaceType = Type.GetType("solid_project_1.ITradeParser, solid-project-1");
+    [Test]
+    public void ITradeParser_Interface_Should_Exist()
+    {
+        var interfaceType = Type.GetType("solid_project_1.ITradeParser, solid-project-1");
 
-            Assert.That(interfaceType, Is.Not.Null);
-            Assert.That(interfaceType.IsInterface, Is.True);
-        }
+        Assert.That(interfaceType, Is.Not.Null);
+        Assert.That(interfaceType.IsInterface, Is.True);
+    }
 
-        [Test]
-        public void ITradeParser_Should_Have_Parse_Method()
-        {
-            var interfaceType = Type.GetType("solid_project_1.ITradeParser, solid-project-1");
+    [Test]
+    public void ITradeParser_Should_Have_Parse_Method()
+    {
+        var interfaceType = Type.GetType("solid_project_1.ITradeParser, solid-project-1");
 
-            var method = interfaceType?.GetMethod("Parse");
+        var method = interfaceType?.GetMethod("Parse");
 
-            Assert.That(method, Is.Not.Null);
-            Assert.That(method.ReturnType, Is.EqualTo(typeof(List<TradeRecord>)));
+        Assert.That(method, Is.Not.Null);
+        Assert.That(method.ReturnType, Is.EqualTo(typeof(List<TradeRecord>)));
 
-            var parameters = method.GetParameters();
-            Assert.That(parameters.Length, Is.EqualTo(1));
-            Assert.That(parameters[0].ParameterType, Is.EqualTo(typeof(List<string>)));
-        }
+        var parameters = method.GetParameters();
+        Assert.That(parameters.Length, Is.EqualTo(1));
+        Assert.That(parameters[0].ParameterType, Is.EqualTo(typeof(List<string>)));
+    }
 
-        [Test]
-        public void ITradeStorage_Interface_Should_Exist()
-        {
-            var interfaceType = Type.GetType("solid_project_1.ITradeStorage, solid-project-1");
+    [Test]
+    public void ITradeStorage_Interface_Should_Exist()
+    {
+        var interfaceType = Type.GetType("solid_project_1.ITradeStorage, solid-project-1");
 
-            Assert.That(interfaceType, Is.Not.Null);
-            Assert.That(interfaceType.IsInterface, Is.True);
-        }
+        Assert.That(interfaceType, Is.Not.Null);
+        Assert.That(interfaceType.IsInterface, Is.True);
+    }
 
-        [Test]
-        public void ITradeStorage_Should_Have_Persist_Method()
-        {
-            var interfaceType = Type.GetType("solid_project_1.ITradeStorage, solid-project-1");
+    [Test]
+    public void ITradeStorage_Should_Have_Persist_Method()
+    {
+        var interfaceType = Type.GetType("solid_project_1.ITradeStorage, solid-project-1");
 
-            var method = interfaceType?.GetMethod("Persist");
+        var method = interfaceType?.GetMethod("Persist");
 
-            Assert.That(method, Is.Not.Null);
-            Assert.That(method.ReturnType, Is.EqualTo(typeof(void)));
+        Assert.That(method, Is.Not.Null);
+        Assert.That(method.ReturnType, Is.EqualTo(typeof(void)));
 
-            var parameters = method.GetParameters();
-            Assert.That(parameters.Length, Is.EqualTo(1));
-            Assert.That(parameters[0].ParameterType, Is.EqualTo(typeof(List<TradeRecord>)));
-        }
+        var parameters = method.GetParameters();
+        Assert.That(parameters.Length, Is.EqualTo(1));
+        Assert.That(parameters[0].ParameterType, Is.EqualTo(typeof(List<TradeRecord>)));
     }
 }
